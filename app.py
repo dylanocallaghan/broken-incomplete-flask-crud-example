c8cc5a9d5b9563a87ae7cfe76b06bcfa68d995cb
Ubuntu <C21752715@C21752715.ealgowhpz41u3jkslt1drjr2rc.zx.internal.cloudapp.net>
2024-08-05 14:39:54 +0000

    Update Flask app with Cloud SQL connection and add Dockerfile

--- app.py
+++ app.py
@@ -1,49 +1,46 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
-mysql = MySQL()
+
 app = Flask(__name__)
 CORS(app)
-# My SQL Instance configurations
-# Change these details to match your instance configurations
-app.config['MYSQL_USER'] = 'A'
-app.config['MYSQL_PASSWORD'] = 'B'
-app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'db.mydomain.ie'
-mysql.init_app(app)
 
-@app.route("/add") #Add Student
+# MySQL instance configurations
+app.config['MYSQL_USER'] = 'crud_user'
+app.config['MYSQL_PASSWORD'] = 'QLPM6T5vSb23bbDw'
+app.config['MYSQL_DB'] = 'flask_crud_db'
+# Use Unix socket for Cloud SQL instance connection
+app.config['MYSQL_HOST'] = '/cloudsql/your-project-id:europe-west2:flask-crud-instance'
+mysql = MySQL(app)
+
+@app.route("/add")  # Add Student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    name = request.args.get('name')
+    email = request.args.get('email')
+    cur = mysql.connection.cursor()  # create a connection to the SQL instance
+    s = """INSERT INTO students(studentName, email) VALUES (%s, %s)"""
+    cur.execute(s, (name, email))  # execute an SQL statement
+    mysql.connection.commit()
+    cur.close()
+    return '{"Result":"Success"}'
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
-def read(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/")  # Default - Show Data
+def read():
+    cur = mysql.connection.cursor()  # create a connection to the SQL instance
+    cur.execute('''SELECT * FROM students''')  # execute an SQL statement
+    rv = cur.fetchall()  # retrieve all rows returned by the SQL statement
+    Results = []
+    for row in rv:  # format the output results and add to return string
+        Result = {}
+        Result['Name'] = row[1].replace('\n', ' ')
+        Result['Email'] = row[2]
+        Result['ID'] = row[0]
+        Results.append(Result)
+    cur.close()
+    response = json.dumps(Results)
+    return response
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    app.run(host='0.0.0.0', port=8080)  # run the Flask app at port 8080
 
