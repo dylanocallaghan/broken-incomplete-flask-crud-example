e0142ae71c787666e9ad22acba08431c8e493f20
Ubuntu <C21752715@C21752715.ealgowhpz41u3jkslt1drjr2rc.zx.internal.cloudapp.net>
2024-08-05 15:10:16 +0000

    corrected app.py details

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 CORS(app)
 
 # MySQL instance configurations
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = 'crud_user'
 app.config['MYSQL_PASSWORD'] = 'QLPM6T5vSb23bbDw'
-app.config['MYSQL_DB'] = 'flask_crud_db'
+app.config['MYSQL_DB'] = 'flask-crud-db'
 # Use Unix socket for Cloud SQL instance connection
 app.config['MYSQL_HOST'] = '/cloudsql/your-project-id:europe-west2:flask-crud-instance'
 mysql = MySQL(app)
